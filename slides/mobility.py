ad637f7c6e8314ab1afd567fbec5f43676047849
Shaoor Munir <shaoormunir@gmail.com>
2023-11-20 21:27:32 -0800

    Bug fixes

--- slides/mobility.py
+++ slides/mobility.py
@@ -3,8 +3,6 @@ from dash import html, dcc, Input, Output, State
 from server import app
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
 
